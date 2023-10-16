5ceac485b2766783c23521e760313a62cb3301b1
Andrei Marius <amha@sitecampaign.com>
2023-10-16 16:41:14 +0200

    done

--- CIT_2023_Livecode2/EchoServer/ServerProgram.cs
+++ CIT_2023_Livecode2/EchoServer/ServerProgram.cs
@@ -53,6 +53,127 @@ while (true)
         client.MyWrite(JsonSerializer.Serialize(res));
     }
 
+    if (requestObj?.Method == "read" && requestObj?.Path?.Length > 17)
+    {
+        try
+        {
+            var res = new Response
+            {
+                Status = "1 Ok",
+                Body = JsonSerializer.Serialize(categories[Int32.Parse(requestObj.Path.Substring(16)) - 1])
+            };
+
+            client.MyWrite(JsonSerializer.Serialize(res));
+            Console.WriteLine("1 Ok");
+            return;
+        }
+        catch (Exception)
+        {
+            var res = new Response
+            {
+                Status = "5 not found",
+            };
+
+            client.MyWrite(JsonSerializer.Serialize(res));
+            Console.WriteLine("5 not found");
+            return;
+        }
+    }
+
+    if (requestObj.Method == "read" && requestObj.Path == "/api/categories")
+    {
+        Console.WriteLine("1 Ok");
+        var res = new Response
+        {
+            Status = "1 Ok",
+            Body = JsonSerializer.Serialize(categories)
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+        return;
+    }
+
+    if (requestObj.Path != $"/api/categories/1" ||
+      requestObj.Path != $"/api/categories/2" ||
+      requestObj.Path != $"/api/categories/3"
+      )
+    {
+        Console.WriteLine("4 Bad Request");
+        var res = new Response
+        {
+            Status = "4 Bad Request"
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
+    var requestBody = JsonConvert.DeserializeAnonymousType(requestObj.Body, new { Name = "" });
+    if (string.IsNullOrWhiteSpace(requestBody.Name))
+    {
+        Console.WriteLine("4 Bad Request");
+        var res = new Response
+        {
+            Status = "4 Bad Request"
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
+    if (!requestObj.Path.Contains("/api/categories/"))
+    {
+        Console.WriteLine("4 Bad Request");
+        var res = new Response
+        {
+            Status = "4 Bad Request"
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
+    if (!string.IsNullOrWhiteSpace(requestObj.Body))
+    {
+        Console.WriteLine("body exists");
+        var res = new Response
+        {
+            Body = requestObj.Body
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
+    if (string.IsNullOrWhiteSpace(requestObj?.Body))
+    {
+        Console.WriteLine("missing body");
+        var res = new Response
+        {
+            Status = "missing body"
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
+    if (!HelperMethods.IsValidJson(requestObj.Body))
+    {
+        Console.WriteLine("illegal body");
+        var res = new Response
+        {
+            Status = "illegal body"
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
+    if (string.IsNullOrWhiteSpace(requestObj?.Date))
+    {
+        Console.WriteLine("missing date");
+        var res = new Response
+        {
+            Status = "missing date"
+        };
+
+        client.MyWrite(JsonSerializer.Serialize(res));
+    }
+
     if (string.IsNullOrWhiteSpace(requestObj?.Method))
     {
         Console.WriteLine("missing method");
@@ -90,17 +211,6 @@ while (true)
         client.MyWrite(JsonSerializer.Serialize(res));
     }
 
-    if (string.IsNullOrWhiteSpace(requestObj?.Date))
-    {
-        Console.WriteLine("missing date");
-        var res = new Response
-        {
-            Status = "missing date"
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-    }
-
     if (requestObj.Date.Length >= DateTimeOffset.Now.ToUnixTimeSeconds().ToString().Length &&
         !int.TryParse(requestObj.Date, out _) &&
         char.IsDigit(requestObj.Date[0]) &&
@@ -113,46 +223,6 @@ while (true)
         };
 
         client.MyWrite(JsonSerializer.Serialize(res));
-    }
-
-    if (requestObj.Method == "read" && requestObj.Path == "/api/categories")
-    {
-        Console.WriteLine("1 Ok");
-        var res = new Response
-        {
-            Status = "1 Ok",
-            Body = JsonSerializer.Serialize(categories)
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-        return;
-    }
-
-    if (requestObj.Method == "read" && requestObj.Path.Length > 17)
-    {
-        try
-        {
-            var res = new Response
-            {
-                Status = "1 Ok",
-                Body = JsonSerializer.Serialize(categories[Int32.Parse(requestObj.Path.Substring(16)) - 1])
-            };
-
-            client.MyWrite(JsonSerializer.Serialize(res));
-            Console.WriteLine("1 Ok");
-            return;
-        }
-        catch (Exception)
-        {   
-            var res = new Response
-            {
-                Status = "5 not found",
-            };
-
-            client.MyWrite(JsonSerializer.Serialize(res));
-            Console.WriteLine("5 not found");
-            return;
-        }
     }
 
     //var result = categories.Find(x => x.cid == Int32.Parse(requestObj.Path.Substring(16)));
@@ -170,17 +240,6 @@ while (true)
     //    return;
     //}
 
-    if (!requestObj.Path.Contains("/api/categories/"))
-    {
-        Console.WriteLine("4 Bad Request");
-        var res = new Response
-        {
-            Status = "4 Bad Request"
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-    }
-
     //string id = requestObj.Path.Substring(16);
 
     //if (requestObj.Path != $"/api/categories/{id}" && !int.TryParse("123", out _))
@@ -193,68 +252,7 @@ while (true)
 
     //    client.MyWrite(JsonSerializer.Serialize(res));
     //}
-    
-    if (requestObj.Path != $"/api/categories/1" ||
-        requestObj.Path != $"/api/categories/2" ||
-        requestObj.Path != $"/api/categories/3"
-        )
-    {
-        Console.WriteLine("4 Bad Request");
-        var res = new Response
-        {
-            Status = "4 Bad Request"
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-    }
-
-    if (string.IsNullOrWhiteSpace(requestObj?.Body))
-    {
-        Console.WriteLine("missing body");
-        var res = new Response
-        {
-            Status = "missing body"
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-    }
-
-    //if (!string.IsNullOrWhiteSpace(requestObj.Body))
-    //{
-    //    Console.WriteLine("body exists");
-    //    var res = new Response
-    //    {
-    //        Body = requestObj.Body
-    //    };
-
-    //    client.MyWrite(JsonSerializer.Serialize(res));
-    //}
-
-    if (!HelperMethods.IsValidJson(requestObj.Body))
-    {
-        Console.WriteLine("illegal body");
-        var res = new Response
-        {
-            Status = "illegal body"
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-    }
-
-    var requestBody = JsonConvert.DeserializeAnonymousType(requestObj.Body, new { Name = "" });
-    if (string.IsNullOrWhiteSpace(requestBody.Name))
-    {
-        Console.WriteLine("4 Bad Request");
-        var res = new Response
-        {
-            Status = "4 Bad Request"
-        };
-
-        client.MyWrite(JsonSerializer.Serialize(res));
-    }
    
-    
-
     Console.WriteLine($"Request: {request}");
 
     var response = request;
